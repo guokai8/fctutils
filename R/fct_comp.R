664357317cea6c63132f116920948d10188b7cd8
Kai Guo <guokai8@gmail.com>
2024-09-24 21:15:42 -0500

    Add files via upload

--- R/fct_comp.R
+++ R/fct_comp.R
@@ -19,7 +19,7 @@
 #'
 #' # Use a custom preprocessing function to convert factor_vec to lower case
 #' fct_pairs(vec, pre_fn = function(x) tolower(trimws(x)))
-#' fct_paris(vec, ref = c("A","C"))
+#' fct_pairs(vec, ref = c("A","C"))
 #' @export
 fct_pairs <- function(factor_vec, ref = NULL, symmetric = TRUE,
                                     include_na = FALSE, include_self = FALSE, filter_fn = NULL,
